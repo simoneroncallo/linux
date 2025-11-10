ff868010d6c2a2533a139b2572df9bc5fa168bc5
simoneroncallo <simoneroncallo1996@gmail.com>
2025-11-10 12:20:45 +0100

    Update backup.sh

--- fedora/backup.sh
+++ fedora/backup.sh
@@ -15,7 +15,7 @@ TIMESTAMP=$(date +%F_%H-%M-%S)
 
 # Directories
 SRC=/home/
-ROOT=/run/media/simone/Backup
+ROOT=/run/media/<user>/Backup
 
 DST="${ROOT}/home"
 DIR="${ROOT}/history/$TIMESTAMP"
@@ -26,7 +26,7 @@ mkdir -p "$DIR"
 # Test
 rsync -avP --dry-run --delete --backup --backup-dir="$DIR" "$SRC" "$DST" --log-file="$LOG"
 
-# Backup (uncomment)
+# Backup
 # rsync -avP --delete --backup --backup-dir="$DIR" "$SRC" "$DST" --log-file="$LOG"
 
 echo "Completed."
